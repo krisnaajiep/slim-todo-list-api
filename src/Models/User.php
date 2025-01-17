84557c66fd935216658c1aef9df4ba455fd721c7
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-17 13:50:11 +0700

    test(user-model): add user creation test with data provider

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,7 +2,62 @@
 
 namespace App\Models;
 
+use App\Database;
+
 class User
 {
-    //
+    private $db;
+    private string $table = 'users';
+
+    public function __construct(Database $db)
+    {
+        $this->db = $db;
+    }
+
+    public function create(array $data): array
+    {
+        $password = password_hash($data["password"], PASSWORD_DEFAULT);
+
+        $this->db->prepare("INSERT INTO $this->table (name, email, password) VALUES (:name, :email, :password)");
+        $this->db->bindParam(':name', $data['name']);
+        $this->db->bindParam(':email', $data['email']);
+        $this->db->bindParam(':password', $password);
+
+        $this->db->execute();
+
+        $id = $this->db->lastInsertId();
+
+        return [
+            'id' => $id,
+            'name' => $data['name'],
+        ];
+    }
+
+    public function get(): array
+    {
+        // Write Code
+
+        return [];
+    }
+
+    public function getAll(): array
+    {
+        // Write Code
+
+        return [];
+    }
+
+    public function update(array $data): array
+    {
+        // Write Code
+
+        return [];
+    }
+
+    public function delete(int $id): bool
+    {
+        // Write Code
+
+        return true;
+    }
 }
--- tests/unit/Models/UserTest.php
+++ tests/unit/Models/UserTest.php
@@ -2,9 +2,45 @@
 
 declare(strict_types=1);
 
+use App\Database;
+use App\Models\User;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
 use PHPUnit\Framework\TestCase;
 
 final class UserTest extends TestCase
 {
-    //
+    private ?User $user;
+    private $db;
+
+    public function setUp(): void
+    {
+        $this->db = $this->createMock(Database::class);
+        $this->user = new User($this->db);
+    }
+
+    #[DataProviderExternal(UserDataProvider::class, 'creationProvider')]
+    public function testCreated(array $data): void
+    {
+        $result = $this->user->create($data);
+
+        $this->assertArrayHasKey('id', $result);
+        $this->assertEquals($data['name'], $result['name']);
+        $this->assertCount(2, $result);
+    }
+}
+
+final class UserDataProvider
+{
+    public static function creationProvider(): array
+    {
+        return [
+            'created data' => [
+                [
+                    'name' => 'John Doe',
+                    'email' => 'john@doe.com',
+                    'password' => 'password'
+                ]
+            ]
+        ];
+    }
 }
