cb42ad9cc9ea813b7e7d9868a57708c88b292f55
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-18 23:24:41 +0700

    feat(validators): add user registration validation rules

---
+++ src/Validators/UserRegisterValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Validators;
+
+use KrisnaAjieP\PHPValidator\Validator;
+
+class UserRegisterValidator
+{
+    public static function validate(array $data): Validator
+    {
+        return Validator::setRules($data, [
+            'name' => ['required', 'alpha', 'min_length:2', 'max_length:50'],
+            'email' => ['required', 'email', 'max_length:254'],
+            'password' => ['required', 'min_length:8', 'max_length:255'],
+            'password_confirmation' => ['required', 'match:password'],
+        ]);
+    }
+}
