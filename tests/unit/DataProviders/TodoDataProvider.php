6e8584d2872959af1fc5885fcf6614f5b3153da2
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-19 06:24:48 +0700

    test(todo-model): add create test with data provider

---
+++ tests/unit/DataProviders/TodoDataProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Test\Unit\DataProviders;
+
+final class TodoDataProvider
+{
+    public static function creationProvider(): array
+    {
+        return [
+            'valid todo data' => [
+                [
+                    'user_id' => rand(1, 100),
+                    'title' => 'Buy Groceries',
+                    'description' => 'Buy milk, eggs, and bread'
+                ]
+            ]
+        ];
+    }
+}
--- tests/unit/Models/TodoTest.php
+++ tests/unit/Models/TodoTest.php
@@ -2,9 +2,32 @@
 
 declare(strict_types=1);
 
+use App\Database;
+use App\Models\Todo;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
 use PHPUnit\Framework\TestCase;
+use Test\Unit\DataProviders\TodoDataProvider;
 
 final class TodoTest extends TestCase
 {
-    //
+    private ?Todo $todo;
+    private $db;
+
+    public function setUp(): void
+    {
+        $this->db = $this->createMock(Database::class);
+        $this->todo = new Todo($this->db);
+    }
+
+    #[DataProviderExternal(TodoDataProvider::class, 'creationProvider')]
+    public function testCreatesTodo(array $data): void
+    {
+        $result = $this->todo->create($data);
+
+        $this->assertIsArray($result);
+        $this->assertCount(3, $result);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertArrayHasKey('title', $result);
+        $this->assertArrayHasKey('description', $result);
+    }
 }
