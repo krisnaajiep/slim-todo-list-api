4870d42dd662ff2d58648f26243c01a0f103833c
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-19 06:26:37 +0700

    feat(todo-model): implement create method with database table creation

--- src/Models/Todo.php
+++ src/Models/Todo.php
@@ -2,7 +2,72 @@
 
 namespace App\Models;
 
+use App\Database;
+
 class Todo
 {
-    //
+    private $db;
+    private string $table = 'todos';
+
+    public function __construct(Database $db = null)
+    {
+        $this->db = $db ?? new Database();
+
+        $this->db->exec("CREATE TABLE IF NOT EXISTS $this->table (
+                         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                         user_id INT(11) UNSIGNED NOT NULL,
+                         title VARCHAR(100) NOT NULL,
+                         description TEXT NOT NULL,
+                         status ENUM('todo', 'in progress', 'done') DEFAULT 'todo',
+                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                         FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+                       )");
+    }
+
+    public function create(array $data): array
+    {
+        $this->db->prepare("INSERT INTO $this->table (user_id, title, description) VALUES (:user_id, :title, :description)");
+        $this->db->bindParam(':user_id', $data['user_id']);
+        $this->db->bindParam(':title', $data['title']);
+        $this->db->bindParam(':description', $data['description']);
+
+        $this->db->execute();
+
+        $id = $this->db->lastInsertId();
+
+        return [
+            'id' => $id,
+            'title' => $data['title'],
+            'description' => $data['description']
+        ];
+    }
+
+    public function get(): array
+    {
+        // Write Code
+
+        return [];
+    }
+
+    public function getAll(): array
+    {
+        // Write Code
+
+        return [];
+    }
+
+    public function update(array $data): array
+    {
+        // Write Code
+
+        return [];
+    }
+
+    public function delete(int $id): bool
+    {
+        // Write Code
+
+        return true;
+    }
 }
