074a816d7cc1673a414ca9cd8d04cb6a3d007f1e
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-18 23:07:59 +0700

    feat(user-model): enhance user creation with error handling for duplicate emails

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,34 +3,54 @@
 namespace App\Models;
 
 use App\Database;
+use Exception;
+use Slim\Exception\HttpBadRequestException;
 
 class User
 {
     private $db;
     private string $table = 'users';
 
-    public function __construct(Database $db)
+    public function __construct(Database $db = null)
     {
-        $this->db = $db;
+        $this->db = $db ?? new Database();
+
+        $this->db->exec("CREATE TABLE IF NOT EXISTS $this->table (
+                         id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                         name VARCHAR(50) NOT NULL,
+                         email VARCHAR(100) NOT NULL,
+                         password VARCHAR(255) NOT NULL,
+                         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                         UNIQUE (email)
+                        )");
     }
 
     public function create(array $data): array
     {
         $password = password_hash($data["password"], PASSWORD_DEFAULT);
 
-        $this->db->prepare("INSERT INTO $this->table (name, email, password) VALUES (:name, :email, :password)");
-        $this->db->bindParam(':name', $data['name']);
-        $this->db->bindParam(':email', $data['email']);
-        $this->db->bindParam(':password', $password);
+        try {
+            $this->db->prepare("INSERT INTO $this->table (name, email, password) VALUES (:name, :email, :password)");
+            $this->db->bindParam(':name', $data['name']);
+            $this->db->bindParam(':email', $data['email']);
+            $this->db->bindParam(':password', $password);
 
-        $this->db->execute();
+            $this->db->execute();
 
-        $id = $this->db->lastInsertId();
+            $id = $this->db->lastInsertId();
 
-        return [
-            'id' => $id,
-            'name' => $data['name'],
-        ];
+            return [
+                'id' => $id,
+                'name' => $data['name'],
+            ];
+        } catch (\PDOException $th) {
+            if ($th->getCode() == 23000) {
+                throw new Exception('Email address already exists.', 409);
+            } else {
+                throw new Exception('Internal server error.');
+            }
+        }
     }
 
     public function get(): array
