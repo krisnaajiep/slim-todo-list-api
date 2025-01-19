8418a0dd8ac8eb15a3338353d6ee7f3c0a135e01
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-20 01:48:08 +0700

    test(todo-controller): add create tests with valid and invalid data

--- tests/unit/Controllers/TodoControllerTest.php
+++ tests/unit/Controllers/TodoControllerTest.php
@@ -2,9 +2,83 @@
 
 declare(strict_types=1);
 
+use App\Models\Todo;
+use Slim\Psr7\Response;
 use PHPUnit\Framework\TestCase;
+use App\Controllers\TodoController;
+use App\JWTHelper;
+use Psr\Http\Message\ServerRequestInterface;
+use Test\Unit\DataProviders\TodoDataProvider;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
 
 final class TodoControllerTest extends TestCase
 {
-    //
+    private ?TodoController $controller;
+    private $model, $request, $jwt;
+    private ?Response $response;
+
+    public function setUp(): void
+    {
+        $this->model = $this->createMock(Todo::class);
+        $this->jwt = $this->createMock(JWTHelper::class);
+        $this->controller = new TodoController($this->model, $this->jwt);
+        $this->request = $this->createMock(ServerRequestInterface::class);
+        $this->response = new Response();
+    }
+
+    #[DataProviderExternal(TodoDataProvider::class, 'creationProvider')]
+    public function testCreateSuccessfully(array $data): void
+    {
+        $this->jwt->expects($this->once())
+            ->method('decode')
+            ->willReturn([
+                'sub' => rand(1, 100)
+            ]);
+
+        $this->request->expects($this->once())
+            ->method('getParsedBody')
+            ->willReturn($data);
+
+        $this->model->expects($this->once())
+            ->method('create')
+            ->willReturn([
+                'id' => rand(1, 100),
+                'title' => $data['title'],
+                'description' => $data['description']
+            ]);
+
+        $response = $this->controller->create($this->request, $this->response, []);
+
+        $result = json_decode((string)$response->getBody(), true);
+
+        $this->assertIsArray($result);
+        $this->assertCount(3, $result);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertArrayHasKey('title', $result);
+        $this->assertArrayHasKey('description', $result);
+        $this->assertSame(201, $response->getStatusCode());
+    }
+
+    #[DataProviderExternal(TodoDataProvider::class, 'invalidCreationProvider')]
+    public function testFailsToCreate(array $data): void
+    {
+        $this->jwt->expects($this->once())
+            ->method('decode')
+            ->willReturn([
+                'sub' => rand(1, 100)
+            ]);
+
+        $this->request->expects($this->once())
+            ->method('getParsedBody')
+            ->willReturn($data);
+
+        $response = $this->controller->create($this->request, $this->response, []);
+
+        $result = json_decode((string)$response->getBody(), true);
+
+        $this->assertIsArray($result);
+        $this->assertCount(1, $result);
+        $this->assertArrayHasKey('errors', $result);
+        $this->assertSame(400, $response->getStatusCode());
+    }
 }
