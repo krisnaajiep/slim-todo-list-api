10852cb599d419dd1ae59cb663f4c3f56a2b07ec
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-21 06:03:06 +0700

    test(todo-model): add delete tests for successful and failed scenarios

--- tests/unit/Models/TodoTest.php
+++ tests/unit/Models/TodoTest.php
@@ -58,17 +58,13 @@ final class TodoTest extends TestCase
             $this->markTestSkipped('Invalid todo data is not for this test');
         }
 
-        if ($id === 2) {
-            $rowCount = 0;
-        } else {
-            $rowCount = 1;
-        }
+        $rowCount = $id === 2 ? 0 : 1;
 
         $this->db->expects($this->once())
             ->method('rowCount')
             ->willReturn($rowCount);
 
-        if ($id === 2) {
+        if (!$rowCount) {
             $this->expectExceptionCode(404);
         } else {
             $this->db->expects($this->once())
@@ -79,5 +75,51 @@ final class TodoTest extends TestCase
         }
 
         $this->todo->update($id, $data);
+    }
+
+    #[DataProviderExternal(TodoDataProvider::class, 'deletionProvider')]
+    public function testDeletesTodo(int $id, int $user_id): void
+    {
+        $this->db->expects($this->once())
+            ->method('rowCount')
+            ->willReturn(1);
+
+        $this->db->expects($this->once())
+            ->method('fetch')
+            ->willReturn(['user_id' => $id]);
+
+        $this->db->expects($this->exactly(2))
+            ->method('execute')
+            ->willReturn(true);
+
+        $result = $this->todo->delete($id, $user_id);
+
+        $this->assertTrue($result);
+    }
+
+    #[DataProviderExternal(TodoDataProvider::class, 'invalidDeletionProvider')]
+    public function testFailsToDeleteTodo(int $id, $user_id): void
+    {
+        $rowCount = $id === 2 ? 0 : 1;
+
+        $this->db->expects($this->once())
+            ->method('rowCount')
+            ->willReturn($rowCount);
+
+        if (!$rowCount) {
+            $this->expectExceptionCode(404);
+        } else {
+            $this->db->expects($this->once())
+                ->method('fetch')
+                ->willReturn(['user_id' => $id]);
+
+            $this->expectExceptionCode(403);
+        }
+
+        $this->db->expects($this->once())
+            ->method('execute')
+            ->willReturn(true);
+
+        $this->todo->delete($id, $user_id);
     }
 }
