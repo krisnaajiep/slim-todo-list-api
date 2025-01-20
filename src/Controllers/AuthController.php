9ccf5d1a9df3cd7e400dab8d90862b798731fcd0
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-20 14:08:21 +0700

    refactor(auth): refactor JWT handling by introducing JWTHelper for encoding and decoding

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
+use App\JWTHelper;
 use App\Models\User;
 use App\Validators\UserLoginValidator;
 use App\Validators\UserRegisterValidator;
-use Firebase\JWT\JWT;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpInternalServerErrorException;
@@ -79,23 +79,13 @@ class AuthController
 
     private function respondWithToken(array $user = [])
     {
-        $key = 'exampe_key';
-        $exp = 3600;
+        $ttl = 3600;
 
-        $payload = [
-            'iat' => time(),
-            'exp' => time() + $exp,
-            'jti' => bin2hex(random_bytes(16)),
-            'sub' => $user['id'],
-            'name' => $user['name'],
-            'access' => true,
-        ];
-
-        $jwt = JWT::encode($payload, $key, 'HS256');
+        $jwt = (new JWTHelper())->encode($user, $ttl, true);
 
         return [
             'access_token' => $jwt,
-            'expires_in' => $exp,
+            'expires_in' => $ttl,
         ];
     }
 }
